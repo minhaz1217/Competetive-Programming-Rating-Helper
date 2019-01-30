684827a5fe42db2fb603566f78b7bd803cd1fd4d
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2019-01-30 20:08:36 +0600

    added front end for /api/cf/ratedcontest
    gets specific contest infomration, contest type and count can be set

--- routes/cfRouter.js
+++ routes/cfRouter.js
@@ -26,8 +26,26 @@ router.route("/")
 router.route("/ratedcontest")
     .get(function(req,res){
         
+        res.render("ratedcontest", {title: "CODEFORCES"});
+
+    })
+    .post(function(req,res){
         var urlGetRatedContest = "https://codeforces.com/api/contest.list?gym=false";   
         var receivedData = "";
+        var ratingsFor = Array();
+        var test = "";
+        var contestCount = Number(req.body.count);
+        if(req.body.div1){
+            ratingsFor.push("Div. 1");
+        }
+        
+        if(req.body.div2){
+            ratingsFor.push("Div. 2");
+        }
+        if(req.body.div3){
+            ratingsFor.push("Div. 3");
+        }
+        
         https.get(urlGetRatedContest, (resp) =>{
             resp.on('data', (chunk) => {
                 receivedData += chunk;
@@ -35,21 +53,28 @@ router.route("/ratedcontest")
             resp.on("end", () =>{
                 console.log("Data send finished");
                 var contestInfo = JSON.parse(receivedData);
+                var myContestCount = 0;
+
                 function setCharAt(str,index,chr) {
                     if(index > str.length-1) return str;
                     return str.substr(0,index) + chr + str.substr(index+1);
                 }
-                
                 if(contestInfo.status == "OK"){
-                      var outputData = '{"result": [';
+                    var outputData = '{"result": [';
                     contestInfo.result.forEach(function(contest){
-                      if(contest.phase == "FINISHED"){
-                        var name = contest.name;
-                        var id = contest.id;
-                        if(name.indexOf("Div. 3")>=0 || name.indexOf("Div. 2")>=0  || name.indexOf("Div. 1")>=0 ){
-                          outputData += '{"id" : '+id + ', "name" : "' + name + '"},';
+                        if(contestCount > myContestCount){
+                            if(contest.phase == "FINISHED"){
+                                var name = contest.name;
+                                var id = contest.id;
+                                for(var rtname =0; rtname< ratingsFor.length;rtname++){
+                                    if(name.indexOf(ratingsFor[rtname])>=0){
+                                        outputData += '{"id" : '+id + ', "name" : "' + name + '"},';
+                                        myContestCount++;
+                                        break;
+                                    }
+                                }
+                            }
                         }
-                      }
                     });
                   if(outputData[outputData.length-1] == ','){
                     outputData = setCharAt(outputData, outputData.length-1, "");
@@ -63,6 +88,7 @@ router.route("/ratedcontest")
                 //res.send(JSON.parse(receivedData));
             })
         });
-    })    
+        
+    })
 ;
 module.exports  = router;
