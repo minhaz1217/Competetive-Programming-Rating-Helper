536f61c05234a35e4dd712a8c10ee73b2e65886e
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2019-01-30 15:11:38 +0600

    got response back from google,
    on /api/cf endpoint

--- routes/cfRouter.js
+++ routes/cfRouter.js
@@ -1,10 +1,25 @@
 var express = require("express");
 var router = express();
+var https = require("https");
 
 router.route("/")
     .get(function(req, res){
         res.send("This is the cf api")
+        var myData = "";
+        https.get("https://www.google.com", (resp) =>{
+            console.log('statusCode:', res.statusCode);
+            console.log('headers:', res.headers);
+                myData += chunk;
+              });
+            resp.on("end", () =>{
+                console.log("Data send finished");
+                //console.log(JSON.parse(myData).explanation);
+                res.send(myData);
+            })
+        });
     })
+
+
     .post(function(req,res){
         res.send(req.body);
     })
