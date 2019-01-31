51e306ec924c5eeba5d248230446168c9e28f2f2
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2019-02-01 00:48:49 +0600

    list generation successful
    on: /api/cf/contestattended

--- routes/cfRouter.js
+++ routes/cfRouter.js
@@ -53,7 +53,97 @@ function getRatedContest(ratingsFor, contestCount, callback){
         })
     }); 
 }
+function getRecentRatings(name, neededCount, callback){
+    var ratingsUrl = "https://codeforces.com/api/user.rating?handle=";
+    var receivedData = "";
+    var outputData = "";
+    https.get(ratingsUrl+ name, (resp) =>{
+        resp.on('data', (chunk) => {
+            receivedData += chunk;
+          });
+        resp.on("end", () =>{
+            function setCharAt(str,index,chr) {
+                if(index > str.length-1) return str;
+                return str.substr(0,index) + chr + str.substr(index+1);
+            }
+            var userInfo = JSON.parse(receivedData);
+            var myRatingsCount = 0;
 
+            if(userInfo.status == "OK"){
+                outputData += '{"name": "' +name+ '","contest": [';    
+                for(var i=userInfo.result.length-1;i>=0;i--){
+                    var contestId = userInfo.result[i].contestId;
+                    outputData = outputData + contestId +",";
+                    myRatingsCount++;
+                    if(myRatingsCount >= neededCount){
+                        break;
+                    }
+                }
+                if(outputData[outputData.length-1] == ','){
+                    outputData = setCharAt(outputData, outputData.length-1, "");
+                }
+                outputData += ']}';
+                callback(outputData);
+            }else{
+                callback("ERROR");
+            }
+
+        })
+        
+    });
+    
+}
+function getFullList(names, needCount, callback){
+    var count = 0;
+    var i =0;
+    var outputData = '{"result" : [';
+    names.forEach( function(name){
+        getRecentRatings(name.trim(), needCount+5, function(contestList){
+            if(contestList != "ERROR"){
+                if(i>0){
+                    outputData += ',' + contestList + '';
+                }else{
+                    outputData += '' + contestList + '';
+                }
+            }
+            i++;
+            if(i == names.length){
+                outputData += ']}';
+                callback(outputData);
+            }
+        });
+    } );
+}
+
+function contestAttended(names, ratingsFor, count, callback){
+    var name, count,str=' {"result":[ ',indexCount=0;
+    getRatedContest( ratingsFor, count, (ratedContests)=>{
+        var contests = JSON.parse(ratedContests);
+        getFullList(names, count, (fullList)=>{
+            var nameList = JSON.parse(fullList);
+            nameList.result.forEach(function(user){
+                name = user.name;
+                count = 0;
+                for(var i=0;i<user.contest.length;i++){
+                    for(var j=0;j<contests.result.length;j++){
+                        if(contests.result[j].id == user.contest[i]){
+                            count++;
+                            break;
+                        }
+                    }
+                }
+                if(indexCount>0){
+                    str += ',';
+                }
+                indexCount++;
+                str += ' { "name" :"' +name + '", "attended" : ' + count + ' } '
+            })
+
+            str += ']}';
+            callback(str);
+        })
+    });
+}
 
 router.route("/")
     .get(function(req, res){
@@ -78,9 +168,7 @@ router.route("/")
 // returns a list of rated contest,depending on count and selected divisions
 router.route("/ratedcontest")
     .get(function(req,res){
-        
         res.render("ratedcontest", {title: "CODEFORCES"});
-
     })
     .post(function(req,res){
         var ratingsFor = Array();
@@ -117,79 +205,51 @@ router.route("/userrecentcontest")
 })
 //post method of : http://localhost:3000/api/cf/userrecentcontest
 .post(function(req,res){
-    function getRecentRatings(name, neededCount, callback){
-        var ratingsUrl = "https://codeforces.com/api/user.rating?handle=";
-        var receivedData = "";
-        var outputData = "";
-        https.get(ratingsUrl+ name, (resp) =>{
-            resp.on('data', (chunk) => {
-                receivedData += chunk;
-              });
-            resp.on("end", () =>{
-                function setCharAt(str,index,chr) {
-                    if(index > str.length-1) return str;
-                    return str.substr(0,index) + chr + str.substr(index+1);
-                }
-                var userInfo = JSON.parse(receivedData);
-                var myRatingsCount = 0;
-    
-                if(userInfo.status == "OK"){
-                    outputData += '{"name": "' +name+ '","contest": [';    
-                    for(var i=userInfo.result.length-1;i>=0;i--){
-                        var contestId = userInfo.result[i].contestId;
-                        outputData = outputData + contestId +",";
-                        myRatingsCount++;
-                        if(myRatingsCount >= neededCount){
-                            break;
-                        }
-                    }
-                    if(outputData[outputData.length-1] == ','){
-                        outputData = setCharAt(outputData, outputData.length-1, "");
-                    }
-                    outputData += ']}';
-                    callback(outputData);
-                }else{
-                    callback("ERROR");
-                }
-    
-            })
-            
-        });
-        
-    }
-    function getFullList(names, needCount, callback){
-        var count = 0;
-        var i =0;
-        var outputData = "";
-        names.forEach( function(name){
-            getRecentRatings(name.trim(), needCount, function(contestList){
-                if(contestList != "ERROR"){
-                    if(i>0){
-                        outputData += ',' + contestList + '';
-                    }else{
-                        outputData += '' + contestList + '';
-                    }
-                }
-                i++;
-                if(i == names.length){
-                    callback(outputData);
-                }
-            });
-        } );
-    }
-
-
-
-
     var names = (req.body.names).split("\r");
     //return res.status(200).send(JSON.parse('{"contest": [1100,1102,1099,1096,1095,1092,1084,1088,1056,1080,1028,1027,1023,1016,1005]}'));
     // gets users ratings changes, so as a result gets recent contest
-    var neededCount = Number(10) + 5;
-    var receivedData = "";
-    var outputData = '{"result" : [';
+    var neededCount = Number(10);
     getFullList(names, neededCount, function(msg){
-        outputData += msg +']}';
-        res.send(JSON.parse(outputData));
+        res.send(JSON.parse(msg));
+    });
+})
+;
+//http://localhost:3000/api/cf/contestattended
+router.route("/contestattended")
+.get(function(req, res){
+
+    res.render("contestattended");
+
+
+
+    /*
+    //res.send("HELLO");
+    var ratingsFor = Array();
+    ratingsFor.push("Div. 1");
+    ratingsFor.push("Div. 2");
+    ratingsFor.push("Div. 3");
+    var names = ['minhaz1217', 'anik_ewu'];
+    var contestCount = 10;
+    //res.send(contestCount);
+    */
+})
+.post(function(req,res){
+
+    var ratingsFor = Array();
+    var contestCount = Number(req.body.count);
+    var names = (req.body.names).split("\r");
+    if(req.body.div1){
+        ratingsFor.push("Div. 1");
+    }
+    
+    if(req.body.div2){
+        ratingsFor.push("Div. 2");
+    }
+    if(req.body.div3){
+        ratingsFor.push("Div. 3");
+    }
+    contestAttended(names, ratingsFor, contestCount, (attended)=>{
+       res.send(JSON.parse(attended));
     });
 })
 ;
